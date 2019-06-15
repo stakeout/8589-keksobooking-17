d767ccefbae121aefe3556adb53d552fe5feab4d
Vadim Novash <novash@tut.by>
2019-06-15 15:59:16 +0300

    module-3 optimization

--- js/main.js
+++ js/main.js
@@ -2,35 +2,52 @@
 
 var arrayCount = 8;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-var getArrayRandomValue = function (array) {
-  return array[Math.floor(Math.random() * array.length)];
-};
-
-var randomFloatNumber = function (min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = rand.toFixed(2);
+var getRandomInteger = function (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1)
+  rand = Math.round(rand);
   return rand;
-};
+}
 
 var getArrayOfPinObjects = function (countOfArrays) {
   var pins = [];
   for (var i = 0; i < countOfArrays; i++) {
     var pin = {
       'author': {
-        'avatar': 'img/avatars/user/0' + (i + 1) + '.png',
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png',
       },
       'offer': {
-        'type': getArrayRandomValue(TYPES)
+        'type': getRandomInteger(0, TYPES.length)
       },
       'location': {
-        'x': randomFloatNumber(1, 100),
-        'y': randomFloatNumber(130, 630)
+        'x': getRandomInteger(1, 1200),
+        'y': getRandomInteger(130, 630)
       }
     };
     pins.push(pin);
   }
   return pins;
 };
-getArrayOfPinObjects(arrayCount);
+
+var setPinInfo = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.left = pin.location.x + 'px';
+  pinElement.style.top = pin.location.y + 'px';
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = '';
+  return pinElement;
+};
+
+var renderAllPins = function (arrayOfPins) {
+  var pinsContainer = document.querySelector('.map__pins');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < arrayOfPins.length; i++) {
+    pinsContainer.appendChild(fragment.appendChild(setPinInfo(arrayOfPins[i])));
+  }
+};
+
+renderAllPins(getArrayOfPinObjects(arrayCount));
+
+document.querySelector('.map').classList.remove('map--faded');
 
